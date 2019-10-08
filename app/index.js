e7801ff510700a99202d98381a2afc9dfc1386f0
Eric <eric@ericblohm.codes>
2019-10-08 16:25:49 -0400

    add base game info

--- app/index.js
+++ app/index.js
@@ -1,1 +1,40 @@
-console.log('Hello world!');
+import document from 'document';
+
+const roomArray = [
+  'Kitchen',
+  'Bedroom',
+  'Office',
+  'Library',
+  'Bathroom',
+  'Basement',
+  'Garage',
+  'Den',
+  'Attic',
+  'Backyard',
+];
+
+const characterArray = [
+  'The Boss',
+  'The Developer',
+  'The Chef',
+  'The Rabbit',
+  'The Janitor',
+  'The Homeowner',
+  'The Athlete',
+  'The Driver',
+  'The Dog',
+  'The Couch Potato',
+];
+
+const weaponArray = [
+  'Knife',
+  'Rope',
+  'Saw',
+  'Tree Branch',
+  'Fork',
+  'Dinner Plate',
+  'Frying Pan',
+  'Lamp',
+  'Keyboard',
+  'Steel Toe Boots',
+];
