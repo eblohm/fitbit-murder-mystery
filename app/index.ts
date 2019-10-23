834ee99b1f140b647e64dab88958055cc1a8c84e
Eric <eric@ericblohm.codes>
2019-10-23 12:10:06 -0400

    more ui, begin connecting ui to logic

--- app/index.ts
+++ app/index.ts
@@ -2,6 +2,29 @@ import document from 'document';
 import { Murderer } from './gameVariables';
 import { setMurderer } from './gameFunctions';
 
-const murderer: Murderer = setMurderer();
+// Variables
+const newGame = document.getElementById('start-button');
+const titleScreen: any = document.getElementById('title-screen');
+const mainScreen: any = document.getElementById('home-screen');
+const homeList = document.getElementById('game-options');
+const homeListItems = homeList.getElementsByClassName('option-item');
 
-console.log('Hello world!');
+newGame.onclick = function(evt) {
+  const murderer: Murderer = setMurderer();
+  titleScreen.style.display = 'none';
+  mainScreen.style.display = 'inline';
+  console.log('hi');
+};
+
+homeListItems.forEach((element, index) => {
+  let touch = element.getElementById('touch-me');
+  touch.onclick = evt => {
+    console.log(`touched: ${index}`);
+  };
+});
+
+const test = document.getElementById('room-travel').getElementById('touch-me');
+
+test.onclick = evt => {
+  console.log('kliked2');
+};
