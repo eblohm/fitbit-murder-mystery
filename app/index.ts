161387d315161be092913329af41bd035baa9ef2
Eric Blohm <eric@ericblohm.codes>
2019-10-22 21:39:49 -0400

    begin adding ui

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,7 @@
 import document from 'document';
+import { Murderer } from './gameVariables';
 import { setMurderer } from './gameFunctions';
 
-setMurderer();
+const murderer: Murderer = setMurderer();
 
 console.log('Hello world!');
